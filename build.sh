77e214ed20c93d4ba1ef67ffe7c95fd0e453c603
Evan Gamble <egamble@factual.com>
2014-05-16 02:14:28 -0700

    don't cp .gitignore

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ javac -cp "WEB-INF/classes:WEB-INF/lib/mail.jar:WEB-INF/lib/activation.jar:$CATA
 touch WEB-INF/web.xml
 WEBAPP_TARGET=$CATALINA_BASE/webapps/ladder
 sudo mkdir -p $WEBAPP_TARGET
-sudo cp `find . -name .git -prune -or -name '*.java' -prune -or -type f -print` --parents -t $WEBAPP_TARGET
+sudo cp `find . -name '.git*' -prune -or -name '*.java' -prune -or -type f -print` --parents -t $WEBAPP_TARGET
