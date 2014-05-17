6e6e2f02a620f12866b44acf4425a02094e98b52
Evan Gamble <egamble@factual.com>
2014-05-17 00:01:10 -0700

    don't copy over existing web.xml

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@ javac -cp "WEB-INF/classes:WEB-INF/lib/mail.jar:WEB-INF/lib/activation.jar:$CATA
 touch WEB-INF/web.xml
 WEBAPP_TARGET=$CATALINA_BASE/webapps/ladder
 sudo mkdir -p $WEBAPP_TARGET
-sudo cp `find . -name '.git*' -prune -or -name '*.java' -prune -or -type f -print` --parents -t $WEBAPP_TARGET
+sudo cp `find . -name '.git*' -prune -or -name '*.java' -prune -or -name web.xml -prune -or -type f -print` --parents -t $WEBAPP_TARGET
+sudo cp -n WEB-INF/web.xml $WEBAPP_TARGET/WEB-INF
+
